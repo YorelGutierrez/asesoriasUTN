20046cce6251f5695ef7f469b62fc1d39cdc69ce
YorelGutierrez <fetillorel@gmail.com>
2026-03-31 18:12:13 -0700

    seeders
    seeders

--- app/Models/carreras.php
+++ app/Models/carreras.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\grupos;
 
 class carreras extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'clave',
+        'logo',
+    ];
+
+    // Relación con grupos (una carrera tiene muchos grupos)
+    public function grupos()
+    {
+        return $this->hasMany(grupos::class);
+    }
 }
